ce3303468466d2f81cd52cf3838b60d05c881f62
Nicholas Guriev <guriev-ns@ya.ru>
2016-05-27 12:01:08 +0300

    delete, фильтрация, skip, limit

--- libs/articles.js
+++ libs/articles.js
@@ -1,7 +1,16 @@
 var db = require("./db");
 
 export async function list(ctx) {
-	ctx.body = await db.ArticleModel.find();
+	var params = Object.assign({ showAll: false, skip: 0, limit: 10 }, ctx.query);
+	ctx.assert(params.skip >= 0, 400, "Parameter skip must be non-negative");
+	ctx.assert(params.limit >= 0, 400, "Parameter limit must be non-negative");
+	ctx.body = await db.ArticleModel
+		.find(params.showAll ? new Object() : { invisible: false })
+		.sort("-createdAt")
+		.skip(params.skip)
+		.limit(params.limit)
+		.select(params.showAll ? "" : "-invisible")
+	;
 }
 
 export async function post(ctx) {
@@ -11,7 +20,7 @@ export async function post(ctx) {
 		text: ctx.request.body.text
 	});
 	await article.save();
-	ctx.status = 200;
+	ctx.status = 201;
 }
 
 export function get(ctx) {
@@ -22,6 +31,7 @@ export function update(ctx) {
 	ctx.status = 501;
 }
 
-export function delete_(ctx) {
-	ctx.status = 501;
+export async function delete_(ctx) {
+	await db.ArticleModel.findByIdAndUpdate(ctx.params.id, { invisible: true });
+	ctx.status = 200;
 }
--- libs/db.js
+++ libs/db.js
@@ -7,7 +7,7 @@ mongoose.connection.on("error", err => console.log("Db error: ", err.message));
 mongoose.connection.once("open", () => console.log("Connected to db"));
 
 var User = new mongoose.Schema({
-	login: { type: String, required: true },
+	login: { type: String, required: true, unique: true },
 	passw: { type: String, required: true }
 });
 
@@ -17,7 +17,8 @@ var Article = new mongoose.Schema({
 	text: { type: String, required: true },
 	createdAt: { type: Date, default: Date.now },
 	modifiedAt: { type: Date, default: Date.now },
-	modifiedCounter: { type: Number, default: Number }
+	modifiedCounter: { type: Number, default: 0 },
+	invisible: { type: Boolean, default: false }
 });
 
 export var ArticleModel = mongoose.model("Article", Article);
